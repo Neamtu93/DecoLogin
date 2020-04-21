98968ecac540acd20f5dfd9edfcbb18dfa3e5710
Neamtu93 <42642669+Neamtu93@users.noreply.github.com>
2020-04-21 22:25:15 +0300

    Have created a ClassDecorator

---
+++ app/config.py
@@ -0,0 +1,17 @@
+class Config:
+    logged={
+        "tried": "none",
+        "user":"16f007d673a3dc8c25f3e9e77448f3520359670b",
+    }
+    cfg={
+        "DISPLAY":1,
+        "SYSTEM":"none",
+        "USER":[{
+            "name":"george",
+            "pass_req":True,
+            "hassed_passwd":"16f007d673a3dc8c25f3e9e77448f3520359670b"
+
+        },
+
+            ],
+        }
---
+++ app/customError.py
@@ -0,0 +1,5 @@
+class NoUserName(Exception):
+    """We can't validate this user"""
+
+class NoGoodPassword(Exception):
+    """Wrogn password provided"""
